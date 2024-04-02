942f749bfaa00b1a84f9385513f277c6f986f007
Sciurdae <2112527123@qq.com>
2024-04-02 22:46:50 +0800

    Site updated: 2024-04-02 22:46:50

--- js/duration.js
+++ js/duration.js
@@ -1,14 +1,34 @@
-var now = new Date();
-function createtime() {
-    var grt= new Date("04/02/2024 15:30:00");//在此处修改你的建站时间，格式：月/日/年 时:分:秒
-    now.setTime(now.getTime()+250);
-    days = (now - grt ) / 1000 / 60 / 60 / 24; dnum = Math.floor(days);
-    hours = (now - grt ) / 1000 / 60 / 60 - (24 * dnum); hnum = Math.floor(hours);
-    if(String(hnum).length ==1 ){hnum = "0" + hnum;} minutes = (now - grt ) / 1000 /60 - (24 * 60 * dnum) - (60 * hnum);
-    mnum = Math.floor(minutes); if(String(mnum).length ==1 ){mnum = "0" + mnum;}
-    seconds = (now - grt ) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
-    snum = Math.round(seconds); if(String(snum).length ==1 ){snum = "0" + snum;}
-    document.getElementById("timeDate").innerHTML = "本站已存活 "+dnum+" 天 ";
-    document.getElementById("times").innerHTML = hnum + " 小时 " + mnum + " 分 " + snum + " 秒";
-}
-setInterval("createtime()",250);
+!(function() {
+    /** 计时起始时间，自行修改 **/
+    var start = new Date("2024/04/02 15:00:00");
+  
+    function update() {
+      var now = new Date();
+      now.setTime(now.getTime()+250);
+      days = (now - start) / 1000 / 60 / 60 / 24;
+      dnum = Math.floor(days);
+      hours = (now - start) / 1000 / 60 / 60 - (24 * dnum);
+      hnum = Math.floor(hours);
+      if(String(hnum).length === 1 ){
+        hnum = "0" + hnum;
+      }
+      minutes = (now - start) / 1000 /60 - (24 * 60 * dnum) - (60 * hnum);
+      mnum = Math.floor(minutes);
+      if(String(mnum).length === 1 ){
+        mnum = "0" + mnum;
+      }
+      seconds = (now - start) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
+      snum = Math.round(seconds);
+      if(String(snum).length === 1 ){
+        snum = "0" + snum;
+      }
+      document.getElementById("timeDate").innerHTML = "本站已存活&nbsp"+dnum+"&nbsp天";
+      document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
+      // 设置字体颜色
+      document.getElementById("timeDate").style.color = "#ff0000"; // 红色
+      document.getElementById("times").style.color = "#00ff00"; // 绿色
+    }
+  
+    update();
+    setInterval(update, 1000);
+  })();
